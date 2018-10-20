aec6892d8b7fd50ce2af32b26c879e2c44b830ba
stanojlovic.marko@gmail.com <stanojlovic.marko@gmail.com>
2018-10-20 12:22:32 +0200

    less time to sleep

--- 31_inject_js_code.py
+++ 31_inject_js_code.py
@@ -2,8 +2,8 @@ from selenium import webdriver
 import time 
 driver = webdriver.Chrome()
 driver.get('http://localhost/se/31.html')
-driver.execute_script("document.body.style.zoom = '1.5'")
-time.sleep(3)
+driver.execute_script("document.body.style.zoom = '1.7'")
+time.sleep(1)
 driver.execute_script('document.getElementById("demo").innerHTML = "JavaScript injection from Selenium";')
-time.sleep(3)
+time.sleep(2)
 driver.close()
